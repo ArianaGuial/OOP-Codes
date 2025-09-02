eaf4675b12e5b622d8ff3d8bb52210de6032834f
Ariana <guialmariearianalexi@gmial.com>
2025-09-02 09:52:18 +0800

    fifth commit

---
+++ src/Exercise1.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Exercise1
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("_______________________" );
+        System.out.println("OOP SARI-SARI STORE");
+
+        System.out.println("\nEnter customer name: ");
+        String customerName = sc.nextLine();
+
+        double[] prices = new double[3];
+        double total = 0;
+        int items = 0;
+
+        System.out.println("\nEnter prices for 3 items: ");
+
+        for(int i = 0; i < 3; i++)
+        {
+            System.out.println("Item " + (i + 1) + ": ");
+            prices[i] = sc.nextDouble();
+
+            total  += prices[i];
+
+            items++;
+
+//            System.out.println("Items purchased: " + items);
+        }
+
+        System.out.println("____________________" );
+        System.out.println("RECEIPT");
+        System.out.println("Customer Name: " + customerName + "\n");
+//        System.out.println("\n");
+
+        for(int i = 0; i < 3; i++)
+        {
+            System.out.println("Item " + (i + 1) + ": Php " + prices[i]);
+        }
+
+        System.out.println("\nTotal: Php " + total);
+        System.out.println("____________________" );
+
+        System.out.println("\nClearing items from stock... ");
+
+        while(items > 0)
+        {
+            items--;
+        }
+
+        System.out.println("Purchased items cleared from stock");
+
+        sc.close();
+    }
+}
